426e4b729d6c2abf6add05b41654f493d1fb2f33
agocsarmin <115459668+szt2agoarm@users.noreply.github.com>
2023-10-05 08:05:37 +0200

    Add files via upload

---
+++ Backend/IskolaAPI/IskolaAPI/Controllers/TesztController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IskolaAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TesztController : ControllerBase
+    {
+        //Api/Teszt/Udvozles
+        [HttpGet]
+        [Route("Udvozles/{nev}")]
+        public string Udvozles(string nev = "Felhasználó")
+        {
+            return $"Hello {nev}!";
+        }
+        //Api/Teszt/Negyzete/5
+        //[HttpGet]
+        //1-nél több HTTP végpontnál meg kell adni a ROUTE útvonalat
+        //[Route("Negyzete/{szam}")]
+        [HttpGet("Negyzete/{szam}")]
+        public string Hatvanyozas(double szam)
+        {
+            return Math.Pow(szam, 2).ToString();
+        }
+    }
+}
